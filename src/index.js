956d193d9ffa786676b7ce3a331a1c54eff143f8
mcmlock <milesmorlock100@gmail.com>
2023-12-29 08:37:18 -0500

    Finished lesson 21

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from "react";
 import * as ReactDOM from 'react-dom';
+// Components
+import App from "./App";
 
 const el = document.getElementById('root');
 
 const root = ReactDOM.createRoot(el);
-
-function App() {
-    const inputType = 'number';
-    const minValue = 5
-    return <input style={{ border: '3px solid red' }} type={inputType} min={minValue} />;
-}
 
 root.render(<App />);
